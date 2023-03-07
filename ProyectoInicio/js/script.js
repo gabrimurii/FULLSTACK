30068bde98126b889152fdd4f0fa2804435e2b87
gabrimurii <gabrimurii@gmail.com>
2023-03-07 09:07:02 +0100

    06-03-2023

--- ProyectoInicio/js/script.js
+++ ProyectoInicio/js/script.js
@@ -1,5 +1,1 @@
-// document.querySelector('html').onclick = function() {
-//     alert('Soy un documento html');
-// }
-
 alert('Hola Mundo'); // Saca un mensaje al cargar la página que dice Hola Mundo
